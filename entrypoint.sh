106a72128e1a9ef8d66c228d6c82c0d8004ea9c7
ShashanKK123 <koppar.shashank@gmail.com>
2016-10-17 22:27:50 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,19 @@
 
 echo 'Initializing laravel database'
 
-mysqld_safe <<-EOSQL
---skip-grant-tables
-EOSQL
+##Stop the mysql service
+/etc/init.d/mysql stop
 
-mysql -u root <<-SQL
+##Start mysql service in safe mode in background
+mysqld_safe --skip-grant-tables &
+sleep 10
+ps aux | grep mysql 
+
+##Create databse 
+mysql -uroot <<-SQL
 CREATE DATABASE IF NOT EXISTS laravel_base;
+exit
 SQL
 
-ls -la 
-
+##Start the laravel php application
 cd laravel5-example/ && php artisan migrate && php artisan serve --port 8000 --host 0.0.0.0
